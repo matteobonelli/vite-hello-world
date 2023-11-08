da34da0821570e56578676e6c6ef88eb95c63df0
matteobonelli <matteobonelli08@gmail.com>
2023-11-08 16:07:51 +0100

    added jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <AppTitle />
+  <div>
+    <AppTitle />
+    <AppHero />
+  </div>
 </template>
 
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue'
 
 export default {
-  components: { AppTitle }
+  components: { AppTitle, AppHero }
 }
 </script>
-
-<style lang="css" scoped></style>
+  
+<style lang="css" scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="jumbotron">
+        <div class="container">
+            <h2>il mio jumbotron!</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppHero'
+}
+</script>
+
+<style lang="css" scoped>
+.jumbotron {
+    padding: 30px;
+    background-color: #111;
+    background-image: url('/images/big-bridge.jpg');
+
+}
+
+.container {
+    width: 1170px;
+    height: 600px;
+    margin: 0 auto;
+    padding: 0 15px;
+}
+
+h2 {
+    text-align: center;
+    font-size: 100px;
+    color: white;
+    text-transform: uppercase;
+}
+</style>
